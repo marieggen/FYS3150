082b06e85ea1e1dd4698b43ed96da084b1104fba
Mari Dahl Eggen <marideg@ulrik.uio.no>
2015-10-05 09:08:11 +0200

    jacobi-method not working

--- projects/project_2/jacobi_tridiag.cpp
+++ projects/project_2/jacobi_tridiag.cpp
@@ -14,44 +14,39 @@ void jacobi(int eps, mat& A, mat& S, int n, int m);
 
 int main(int argc,char** argv)
 {
-	int n = 5;
+	int n = 100;
 	int m = n+2;
 	double eps = 10e-8;
 	double rho_min = 0.0;
-	double rho_max = 10.0;
+	double rho_max = 5.0;
 	double h = (rho_max-rho_min)/(m);
 	double h_sq = h*h;
 	double k1 = -1.0/h_sq;
 
 	//Initialize three diagonal matrix
 	mat A = eye<mat>(n,n);
+	vec V(n);
+
 	for(int i=0 ; i<n ; i++){
+		V(i) = (i+1)*h*(i+1)*h; 
 
-		double rho = rho_min + (i+1)*h;
-		double V = rho*rho;
+		A(i,i) = (2.0/h_sq)+V(i);
+		if(i<n-1){
 
-		for(int j=0 ; j<n ; j++){
-			if(i==j){
-
-				A(i,j) = (2.0/h_sq)+V;
-
-				if(j<(n-1)){
-
-				A(i,j+1) = k1;
-				A(i+1,j) = k1;
-				}
-			}
+			A(i,i+1) = k1;
+			A(i+1,i) = k1;
 		}
 	}
 
 	mat eigval = eig_sym(A);
-	cout << eigval << endl;
+	cout << eigval(0) << " , " << eigval(1) << " , " << eigval(2) << endl;
 
 	mat S = eye<mat>(n,n);	
 	mat S_T = strans(S);
 
 	jacobi(eps,A,S,n,m);
 	cout << A(1,1) << " , " << A(2,2) << " , " << A(3,3) << endl;
+
 
 	return 0;
 }
@@ -151,3 +146,4 @@ void rotate(mat& A, mat& S, int k, int l, int n)
 	//cout << "The number of rotations is: " << numRotations << endl;
 	return;
 }
+
